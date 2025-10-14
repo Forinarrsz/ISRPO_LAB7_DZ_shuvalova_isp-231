45e9fc420dca62ffdd05f3e00513255a612569dd
Forinarr <shuvalova0723@list.ru>
2025-10-14 19:14:29 +0300

    1

---
+++ ISRPO_LAB7_DZ/Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISRPO_LAB7_DZ
+{
+    internal class Character
+    {
+        public string Name;
+        public int Health;
+        public int Mana;
+
+        public Character(string name, int health, int mana)
+        {
+            Name = name;
+            Health = health;
+            Mana = mana;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"{Name} атакует");
+        }
+    }
+}
--- ISRPO_LAB7_DZ/Program.cs
+++ ISRPO_LAB7_DZ/Program.cs
@@ -8,18 +8,18 @@
             book kniga = new book("shine", "stephen king", 1977, true);
             defaultbook.Printinfo();
             kniga.Printinfo();
-
+            //2
             BankAccount bank = new BankAccount();
             bank.Deposit(1000);
             bank.snatie(500);
             bank.printinfo();
-
+            //3
             Car cr = new Car();
             cr.Accelerate(120);
             cr.Brake();
             Dice dice6 = new Dice();
             Dice dice12 = new Dice(12);
-
+            //4
             Console.Write("6-гранный: ");
             for (int i = 0; i < 3; i++)
                 Console.Write(dice6.Roll() + (i < 2 ? ", " : "\n"));
@@ -27,6 +27,13 @@
             Console.Write("12-гранный: ");
             for (int i = 0; i < 3; i++)
                 Console.Write(dice12.Roll() + (i < 2 ? ", " : "\n"));
+            //5
+            Character warrior = new Character("Воин", 100, 50);
+            Character mage = new Character("Маг", 80, 120);
+            Character archer = new Character("Лучник", 90, 60);
+            warrior.Attack();
+            mage.Attack();
+            archer.Attack();
 
         }
     }
